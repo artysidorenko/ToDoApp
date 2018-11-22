b4c84393c636fcaa626cd1441f9fc9f2d7dcd897
Arty Sidorenko <26905074+artysidorenko@users.noreply.github.com>
2018-11-22 21:06:56 +0200

    Code up Tape tests and passing functions for logic.js

--- logic.js
+++ logic.js
@@ -1,8 +1,25 @@
 // Part 1. Fill in any missing parts of the todoFunction object!
 // you can access these on todo.todoFunctions
 // For part one we expect you to use tdd
-
-var todoFunctions = {
+ var constTodoArray = [
+   {
+     "id": 999,
+     "description": "This is a placeholder",
+     "done": false
+   },
+   {
+     "id": 747,
+     "description": "Zis is another placeholder",
+     "done": true
+   },
+   {
+     "id": 911,
+     "description": "And this is yet another placeholder",
+     "done": false
+   },
+];
+ var placeholderTodo = {};
+ var todoFunctions = {
   // todoFunctions.generateId() will give you a unique id
   // You do not need to understand the implementation of this function.
   generateId: (function() {
@@ -14,45 +31,59 @@ var todoFunctions = {
 
     return incrementCounter;
   })(),
-  
-  //cloneArrayOfObjects will create a copy of the todos array 
+
+  //cloneArrayOfObjects will create a copy of the todos array
   //changes to the new array don't affect the original
   cloneArrayOfObjects: function(todos) {
     return todos.map(function(todo){
       return JSON.parse(JSON.stringify(todo));
     });
   },
-  
+
   addTodo: function(todos, newTodo) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // returns a new array, it should contain todos with the newTodo added to the end.
     // add an id to the newTodo. You can use the generateId function to create an id.
     // hint: array.concat
+    newArray = todoFunctions.cloneArrayOfObjects(todos);
+    newTodo["id"] = todoFunctions.generateId();
+    return newArray.concat(newTodo);
   },
   deleteTodo: function(todos, idToDelete) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // return a new array, this should not contain any todo with an id of idToDelete
     // hint: array.filter
+    newArray = todoFunctions.cloneArrayOfObjects(todos);
+    return newArray.filter((e)=>{return e.id!=idToDelete;});
   },
   markTodo: function(todos, idToMark) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // in the new todo array, all elements will remain unchanged except the one with id: idToMark
     // this element will have its done value toggled
     // hint: array.map
+    newArray = todoFunctions.cloneArrayOfObjects(todos);
+    return newArray.map((e)=>{
+      let mappedElem = e;
+      if (e.id==idToMark) {mappedElem.done = !e.done;}
+      return mappedElem;
+    });
   },
   sortTodos: function(todos, sortFunction) {
     // stretch goal! Do this last
     // should leave the input arguement todos unchanged (you can use cloneArrayOfObjects)
     // sortFunction will have same signature as the sort function in array.sort
     // hint: array.slice, array.sort
+    newArray = todoFunctions.cloneArrayOfObjects(todos);
+    newArray.sort(sortFunction);
+    return newArray;
   },
 };
 
 
 // Why is this if statement necessary?
 // The answer has something to do with needing to run code both in the browser and in Node.js
-// See this article for more details: 
+// See this article for more details:
 // http://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
 if (typeof module !== 'undefined') {
-  module.exports = todoFunctions;
+  module.exports = { todoFunctions, constTodoArray, placeholderTodo }
 }
--- test.js
+++ test.js
@@ -1,7 +1,128 @@
-var test = require('tape');
-var logic = require('./logic');
+const test = require('tape');
+const { todoFunctions, constTodoArray, placeholderTodo } = require('./logic.js');
 
-test('Example test', function(t) {
-  t.pass();
+const localTodoArray_original = [
+  {
+    "id": 999,
+    "description": "This is a placeholder",
+    "done": false
+  },
+  {
+    "id": 747,
+    "description": "Zis is another placeholder",
+    "done": true
+  },
+  {
+    "id": 911,
+    "description": "And this is yet another placeholder",
+    "done": false
+  }
+];
+
+const localTodoArray_sortedId = [
+  {
+    "id": 747,
+    "description": "Zis is another placeholder",
+    "done": true
+  },
+  {
+    "id": 911,
+    "description": "And this is yet another placeholder",
+    "done": false
+  },
+  {
+    "id": 999,
+    "description": "This is a placeholder",
+    "done": false
+  }
+];
+
+const localTodoArray_sortedDesc = [
+  {
+    "id": 911,
+    "description": "And this is yet another placeholder",
+    "done": false
+  },
+  {
+    "id": 999,
+    "description": "This is a placeholder",
+    "done": false
+  },
+  {
+    "id": 747,
+    "description": "Zis is another placeholder",
+    "done": true
+  }
+];
+
+const localTodoArray_sortedStatus = [
+  {
+    "id": 747,
+    "description": "Zis is another placeholder",
+    "done": true
+  },
+  {
+    "id": 999,
+    "description": "This is a placeholder",
+    "done": false
+  },
+  {
+    "id": 911,
+    "description": "And this is yet another placeholder",
+    "done": false
+  }
+];
+
+test('Test addToDo function', function(t) {
+  let resultArray = todoFunctions.addTodo(constTodoArray, placeholderTodo);
+  t.equal(Array.isArray(resultArray), true, "function should return an array");
+  t.equal(constTodoArray.join(""), localTodoArray_original.join(""), "should leave input argument todos unchanged (warning: only tests for joined array equality");
+  t.equal(placeholderTodo.hasOwnProperty("id"), true, "input argument todo should have a generated ID");
+  t.equal(resultArray[resultArray.length-1], placeholderTodo, "returned array should include the new item at the end")
+  t.end();
+});
+
+test('Test deleteTodo function', function(t) {
+
+  let resultArray = todoFunctions.deleteTodo(constTodoArray, 999);
+  function checkId(array, id) {
+    for (obj in array)  {
+      if (obj.id == id) {return true;}
+    }
+    return false;
+  }
+
+  t.equal(Array.isArray(resultArray), true, "function should return an array");
+  t.equal(constTodoArray.join(""), localTodoArray_original.join(""), "should leave input argument todos unchanged (warning: only tests for joined array equality");
+  t.equal(checkId(resultArray, 999), false, "new array should not have any object with deleted id");
+  t.end();
+});
+
+test('Test markTodo function', function(t) {
+
+  let resultArray = todoFunctions.markTodo(constTodoArray, 999);
+  function selectTodo(array, id) {
+    return array.filter((e)=> {return e.id==id;})[0]
+  }
+
+  t.equal(Array.isArray(resultArray), true, "function should return an array");
+  t.equal(constTodoArray.join(""), localTodoArray_original.join(""), "should leave input argument todos unchanged (warning: only tests for joined array equality");
+  t.equal(selectTodo(resultArray, 999).done,  !selectTodo(constTodoArray, 999).done, "new array should have an opposite done status on the selected object");
+  t.end();
+});
+
+test('Test sortTodos function', function(t) {
+  let testSort = constTodoArray.sort((a, b)=>{return a.id - b.id;});
+  console.log(testSort);
+
+  let idSortFunc = function(a, b) {return a.id - b.id;};
+  let descSortFunc = function(a, b) {return a.description - b.description;};
+  let statusSortFunc = function(a, b) {return a.done - b.done;};
+
+  t.equal(Array.isArray(todoFunctions.sortTodos(constTodoArray, idSortFunc)), true, "function should return an array");
+  t.equal(constTodoArray.join(""), localTodoArray_original.join(""), "should leave input argument todos unchanged (warning: only tests for joined array equality");
+  t.equal(todoFunctions.sortTodos(constTodoArray, idSortFunc).join(""), localTodoArray_sortedId.join(""), "new array should be sorted by id a-z");
+  t.equal(todoFunctions.sortTodos(constTodoArray, descSortFunc).join(""), localTodoArray_sortedDesc.join(""), "new array should be sorted by description a-z");
+  t.equal(todoFunctions.sortTodos(constTodoArray, statusSortFunc).join(""), localTodoArray_sortedStatus.join(""), "new array should be sorted by done status");
   t.end();
 });
